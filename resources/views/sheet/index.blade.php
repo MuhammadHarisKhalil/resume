4d36915740c75d14239a4f42e8329250acb476d4
hariskhalil <haris.khalil@stackupsolutions.com>
2022-06-23 19:28:32 +0500

    design

--- resources/views/sheet/index.blade.php
+++ resources/views/sheet/index.blade.php
@@ -1,118 +1,138 @@
 <!DOCTYPE html>
 
+<html>
+
 <head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Haris Khalil</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+    <title>Spreadsheet in Laravel</title>
 
-    <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
-    <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
 
-    <!-- Vendor CSS Files -->
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />
 
-    <!-- Template Main CSS File -->
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" />
 
-    <!-- =======================================================
-    * Template Name: iPortfolio - v3.7.0
-    * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-    * Author: BootstrapMade.com
-    * License: https://bootstrapmade.com/license/
-    ======================================================== -->
+
+
+
 </head>
 
 <body>
-    <div class="d-flex col-md-12" id="wrapper">
-        <!-- Sidebar-->
-        <div class="border-end bg-white" id="sidebar-wrapper">
-            <div class="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
-            <div class="list-group list-group-flush">
-                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
-            </div>
+
+    <div class="container">
+
+        <div class="card-header bg-secondary dark bgsize-darken-4 white card-header">
+
+            <h4 class="text-white">Google Sheet read and write in Laravel</h4>
+
         </div>
-        <!-- Page content wrapper-->
-        <div id="page-content-wrapper">
-            <!-- Top navigation-->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-                <div class="container-fluid">
-                    <button class="btn btn-primary" id="sidebarToggle">Toggle Menu</button>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation"><span
-                            class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                            <li class="nav-item active"><a class="nav-link" href="#!">Home</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#!">Link</a></li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#!">Action</a>
-                                    <a class="dropdown-item" href="#!">Another action</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#!">Something else here</a>
-                                </div>
-                            </li>
-                        </ul>
+
+        <div class="row ">
+
+            <div class="col-md-6 mt-4">
+
+                <div class="card">
+
+                    <div class="card-header bgsize-primary-4 white card-header">
+
+                        <h4 class="card-title">Customer Form</h4>
+
+                    </div>
+
+                    <div class="card-body">
+
+                        @if ($message = Session::get('success'))
+                            <div class="alert alert-success alert-block">
+
+
+
+
+                                <button type="button" class="close" data-dismiss="alert">×</button>
+
+
+
+
+                                <strong>{{ $message }}</strong>
+
+
+
+
+                            </div>
+
+                            <br>
+                        @endif
+
+                        <form method="POST" action="send-sheet-data">
+                            @csrf
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Name</label>
+                                <input type="text" class="form-control" name="name" id="exampleFormControlInput1"
+                                    placeholder="name">
+                                <label for="exampleFormControlInput1">Company</label>
+                                <input type="text" class="form-control" name="company" id="exampleFormControlInput1"
+                                    placeholder="company">
+                            </div>
+                            <input type="submit" class=" mt-2 btn btn-primary">
+                        </form>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="row justify-content-left col-md-6 mt-4">
+
+                <div class="col-md-12">
+
+                    <div class="card">
+
+                        <div class="card-header bgsize-primary-4 white card-header">
+
+                            <h4 class="card-title">Customer Data Table</h4>
+
+                        </div>
+
+                        <div class="card-body">
+
+                            <div class=" card-content table-responsive">
+
+                                <table id="example" class="table table-striped table-bordered" style="width:100%">
+
+                                    <thead>
+
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Company</th>
+
+                                    </thead>
+
+                                    <tbody>
+                                        @foreach ($data as $value)
+                                            <tr>
+                                                @if (!$loop->first)
+                                                    <th scope="row">{{ $value[0] }}</th>
+                                                    <td>{{ $value[1] }}</td>
+                                                    <td>{{ $value[2] }}</td>
+                                                @endif
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </nav>
-            <!-- Page content-->
-            <div class="container-fluid col-md-10">
-                <form method="POST" action="send-sheet-data">
-                    @csrf
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">Name</label>
-                        <input type="text" class="form-control" name="name" id="exampleFormControlInput1"
-                            placeholder="name">
-                        <label for="exampleFormControlInput1">Company</label>
-                        <input type="text" class="form-control" name="company" id="exampleFormControlInput1"
-                            placeholder="company">
-                    </div>
-                    <input type="submit" class=" mt-2 btn btn-primary">
-                </form>
-                <table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Company</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($data as $value)
-                            <tr>
-                                @if (!$loop->first)
-                                    <th scope="row">{{ $value[0] }}</th>
-                                    <td>{{ $value[1] }}</td>
-                                    <td>{{ $value[2] }}</td>
-                                @endif
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
             </div>
         </div>
     </div>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
 </body>
+
+</html>
